09acca78d4650ed2d9c83a3e88d4bfabc8d48e5f
DeevAlex <alexdematoscaceres@gmail.com>
2023-01-22 18:34:13 -0300

    Atualizações no cronometro

--- CRONOMETRO/assets/js/meuscript3.js
+++ CRONOMETRO/assets/js/meuscript3.js
@@ -8,49 +8,76 @@ function criaHoraSegundos(segundos) {
 }
 
 let h = document.querySelector(".horario")
-const iniciar = document.querySelector(".iniciar")
-const pausar = document.querySelector(".pausar")
-const zerar = document.querySelector(".zerar")
+const iniciar = document.querySelector(".btn-iniciar")
+const pausar = document.querySelector(".btn-pausar")
+const zerar = document.querySelector(".btn-zerar")
 const relogio = document.querySelector(".timer")
 let segundos = 0
 let timer
 
 function iniciaTimer() {
+
     timer = setInterval(function() {
         segundos++
         relogio.innerHTML = criaHoraSegundos(segundos)
     }, 1000)
 
-    iniciar.className = "iniciado"
-    iniciar.innerHTML= "Iniciado"
-    relogio.className = "timer"
-    pausar.classList.remove("pausado")
+    iniciar.classList.add('iniciado')
+    iniciar.innerHTML = 'Iniciado'
 
 }
 
-iniciar.addEventListener('click', function(event) {
+iniciar.addEventListener('click', () => {
+
     clearInterval(timer)
-    iniciar.innerHTML = "Iniciar"
-    pausar.innerHTML = "Pausar"
+
+    if (iniciar.classList.contains('iniciado')) {
+        alert('O Cronômetro Já Está Iniciado')
+    }
+
+    if (pausar.innerHTML === 'Pausado') {
+        pausar.classList.remove('pausado')
+        pausar.innerHTML = 'Pausar'
+        iniciar.classList.add('iniciado')
+        iniciar.innerHTML = 'Iniciado'
+    }
+
     iniciaTimer()
+    
 })
-pausar.addEventListener('click', function(event) {
+
+pausar.addEventListener('click', () => {
+
     clearInterval(timer)
-    relogio.className = "timerPausado"
-    iniciar.className = "reativarButton"
-    iniciar.innerHTML = "Continuar"
-    pausar.innerHTML = "Pausado"
-    pausar.className = "pausado"
+
+    if (pausar.classList.contains('pausado')) {
+        alert('O Cronômetro Já Está Pausado')
+    }
+
+    if (iniciar.classList.contains('iniciado')) {
+        iniciar.classList.remove('iniciado')
+        pausar.classList.add('pausado')
+        iniciar.innerHTML = 'Continuar'
+        pausar.innerHTML = 'Pausado'
+        relogio.classList.add('timerPausado')
+    }
+
 })
-zerar.addEventListener('click', function(event) {
+
+zerar.addEventListener('click', () => {
+
     clearInterval(timer)
     segundos = 0
-    relogio.innerHTML = "00:00:00"
-    relogio.className = "timer"
-    pausar.classList.remove("pausado")
-    iniciar.innerHTML = "Iniciar"
-    pausar.innerHTML = "Pausar"
-    iniciar.className = "button"
+    relogio.innerHTML = '00:00:00'
+
+    if (iniciar.innerHTML === 'Continuar' || iniciar.classList.contains('iniciado')) {
+        iniciar.classList.remove('iniciado')
+        pausar.classList.remove('pausado')
+        iniciar.innerHTML = 'Iniciar'
+        pausar.innerHTML = 'Pausar'
+        relogio.classList.remove('timerPausado')
+    }
+
 })
 
 
