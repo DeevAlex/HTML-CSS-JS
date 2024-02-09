2a51fadb3660c15a32739af282ebd1f5398a0e96
Alex de Matos Caceres <alexdematoscaceres@gmail.com>
2024-02-09 14:58:20 -0300

    Remoção de uma pasta

--- Site 3 - yt/assets/JS/script.js
+++
@@ -1,9 +0,0 @@
-let input = document.querySelector(".campoTexto")
-let quantidadeLetras = document.querySelector(".qtdLetters")
-
-input.addEventListener("keypress", (e) => {
-    quantidadeLetras.textContent = (input.value.length + 1) + "/200"
-    if (input.value.length == 44) {
-        quantidadeLetras.textContent += "\n"
-    }
-})
